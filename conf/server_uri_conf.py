b380352124517f2d0e76cb3556d928ccd1967a82
lszing <lushuzhi1996@163.com>
2021-10-25 09:51:11 +0800

    10.21update

--- conf/server_uri_conf.py
+++ conf/server_uri_conf.py
@@ -1,4 +1,3 @@
 class ServerUriConf:
-    # offlineIP='http://127.0.0.1:8080'
-    offlineIP='http://ware.m.jd.care'
-    onlineIP='https://10.157.131.13:8080'
+    offlineIP = 'http://127.0.0.1:8080'
+    onlineIP = 'http://127.0.0.1:8080'
