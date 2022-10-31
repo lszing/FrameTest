7381af0b476454b622d30a04a01f97a5c8da8f6a
lushuzhi1 <lushuzhi1@jd.com>
2022-10-31 15:16:25 +0800

    delete

--- study/二进制运算.py
+++
@@ -1,9 +0,0 @@
-
-'''
-二进制求和
-'''
-def addBinary(a, b) -> str:
-    print(bin(int(a, 2) + int(b, 2)))
-    return '{0:b}'.format(int(a, 2) + int(b, 2))
-
-print(addBinary('11', '10'))
--- study/输出连续字母长度排名第k个的字母长度.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    dic = {"a": 3, "b": 1, "c": 1, "d": 2}
-    print(sorted(dic, key=lambda x: dic[x]))
-    li = ["123", "97", "5412"]
-    li.sort(reverse=True)
-    print(li)
